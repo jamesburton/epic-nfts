3b497b0f7d3dfaa103384af1bd17e11fa5b2b7b0
James Burton <james@code-consultants.co.uk>
2022-09-13 11:41:31 +0100

    Modified to encode JSON without requiring npoint.io to server JSON

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -35,8 +35,9 @@ contract MyEpicNFT is ERC721URIStorage {
     // _setTokenURI(newItemId, "blah");
     // _setTokenURI(newItemId, "https://api.npoint.io/86ce62f99d90f2d656b1"); // Spongebox Cowboy Pants
     // NB: Edit on https://www.npoint.io/docs/20af4aabe6a0e92de44f // 3476604559_An_HD_photo_of_a_purple_sifaka.png / ipfs://QmPk5vPprBg436yxq1VCcU8LCCVNAzaysPwuxUJ3A28FYE
-    _setTokenURI(newItemId, "https://api.npoint.io/20af4aabe6a0e92de44f"); // 3476604559_An_HD_photo_of_a_purple_sifaka.png / ipfs://QmPk5vPprBg436yxq1VCcU8LCCVNAzaysPwuxUJ3A28FYE
-    
+    //_setTokenURI(newItemId, "https://api.npoint.io/20af4aabe6a0e92de44f"); // 3476604559_An_HD_photo_of_a_purple_sifaka.png / ipfs://QmPk5vPprBg436yxq1VCcU8LCCVNAzaysPwuxUJ3A28FYE
+    // Swapped to encoded JSON without npoint.io hosting
+    _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiMzQ3NjYwNDU1OV9Bbl9IRF9waG90b19vZl9hX3B1cnBsZV9zaWZha2EucG5nIiwKICAgICJkZXNjcmlwdGlvbiI6ICJBIHN0YWJsZSBkaWZmdXNpb24gZ2VuZXJhdGVkIHB1cnBsZSBzaWZha2EgcGhvdG8uIiwKICAgICJpbWFnZSI6ICJpcGZzOi8vUW1QazV2UHByQmc0MzZ5eHExVkNjVThMQ0NWTkF6YXlzUHd1eFVKM0EyOEZZRSIKfQ==");
 
     // Log the activity
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
